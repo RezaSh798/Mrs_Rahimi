5d93703e7bc673a91b0cf80772e04e4a4e031c0e
RezaSh798 <rezakhorrami729@gmail.com>
2020-08-26 16:29:24 +0430

    insert PageTitle component in UserPanel component

--- src/components/pages/UserPanel.vue
+++ src/components/pages/UserPanel.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        Hello user !!
-    </div>
+<section>
+    <page-title :icon="'far fa-user'">
+        <template slot="title">پنل کاربری</template>
+        <template slot="location">پنل کاربری</template>
+    </page-title>
+</section>
 </template>
+
+<script>
+import PageTitle from '../layout/PageTitle.vue'
+
+export default {
+    components: { PageTitle }    
+}
+</script>
