25293c453735d1d48c7fa9e262211de616d1e7c0
RezaSh798 <rezakhorrami729@gmail.com>
2020-09-22 12:16:17 +0330

    deleting slider component and set vueslider :D

--- src/components/pages/Index.vue
+++ src/components/pages/Index.vue
@@ -1,19 +1,49 @@
 <template>
     <section>
-        <my-slider></my-slider>
+        <v-app>
+            <v-carousel
+                cycle
+                height="600"
+                hide-delimiter-background
+                show-arrows-on-hover
+            >
+                <v-carousel-item
+                v-for="(item, i) in items"
+                :key="i"
+                :src="item.src"
+                >
+                </v-carousel-item>
+            </v-carousel>
+        </v-app>
         <my-new-items></my-new-items>
     </section>
 </template>
 
 <script>
 // import components
-import Slider from '../layout/index/Slider.vue'
 import NewItems from '../layout/index/NewItems.vue'
 
 export default {
+    data() {
+        return {
+            items: [
+                {
+                    src: './assets/images/sliders/1.jpg'
+                },
+                {
+                    src: './assets/images/sliders/2.jpg'
+                },
+                {
+                    src: './assets/images/sliders/3.jpg'
+                }
+            ],
+        }
+    },
     components : {
-        'my-slider' : Slider,
         'my-new-items' : NewItems,
     },
+    created() {
+        this.$vuetify.rtl = false;
+    }
 }
 </script>
